c58be53efbfaab888623da93817cd0159d697e68
samriddha poudel <poudelsamriddha46@gmail.com>
2026-05-23 20:32:42 +0545

    added the slider in client section with auto slider also

--- script.js
+++ script.js
@@ -24,4 +24,42 @@ const navSearch = document.getElementById("nav-search");
 
 navSearch.addEventListener("click", (e) => {
      navSearch.classList.toggle("open");
-})
+});
+
+const slides=document.querySelectorAll('.slider-slide');
+const dots=document.querySelectorAll('.slider-dot');
+let currentindex=0;
+
+function showslide(index){
+     slides.forEach((slide, i)=>{
+slide.classList.toggle('active', i === index);
+     });
+
+     dots.forEach((dot, i) =>{
+          dot.classList.toggle('active', i ===index);
+     });
+
+     const sliderwrapper= document.querySelector('.slider-wrapper');
+     sliderwrapper.style.transform= `translateX(-${index * 100}%)`;
+}
+
+
+function nextslide(){
+     currentindex= (currentindex + 1) % slides.length;
+     showslide(currentindex);
+}
+
+dots.forEach((dot, index) =>{
+
+     dot.addEventListener('click',() => {
+          currentindex = index;
+          showslide(currentindex);
+
+     })
+});
+
+showslide(currentindex);
+
+const autoslideinterval = 5000;
+setInterval(nextslide, autoslideinterval);
+
